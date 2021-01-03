58d01ecc47680c5c4ce6bec30a10287febc7aafc
PinkWink <pinkwink.korea@gmail.com>
2021-01-03 13:50:02 +0900

    bug_fix

--- omo_r1mini_bringup/nodes/omo_r1mini_node.py
+++ omo_r1mini_bringup/nodes/omo_r1mini_node.py
@@ -14,6 +14,17 @@ from tf.broadcaster import TransformBroadcaster
 from tf.transformations import quaternion_from_euler, euler_from_quaternion
 from omo_r1mini_bringup.srv import Battery, BatteryResponse
 
+class OdomPose(object):
+   x = 0.0
+   y = 0.0
+   theta = 0.0
+   timestamp = 0
+
+class OdomVel(object):
+   x = 0.0
+   y = 0.0
+   w = 0.0
+
 class PacketHandler:
    def __init__(self):
       port_name = rospy.get_param('~port', '/dev/ttyMotor')
@@ -25,8 +36,8 @@ class PacketHandler:
                                        line_buffering = True)
 
       self.robot_state = {
-            "ENCOD" : [0., 0.],
-            "DIFFV" : [0., 0.],
+            "POSE" : [0., 0.],
+            "VW" : [0., 0.],
             "ODO" : [0., 0.],
             "ACCL" : [0., 0., 0.],
             "GYRO" : [0., 0., 0.],
@@ -75,7 +86,7 @@ class PacketHandler:
             rospy.logwarn("ValueError occupied in parser_R1mini. %s ", raw_data_o)
 
    def set_periodic_info(self):
-      target_info = ['ODO', 'DIFFV', 'ENCOD', 'ACCL', 'GYRO']
+      target_info = ['ODO', 'VW', 'POSE', 'ACCL', 'GYRO']
       for idx, each in enumerate(target_info):
          self.write_port("$cREGI," + str(idx) + "," + each)
 
@@ -90,7 +101,7 @@ class PacketHandler:
       sleep(0.01)
 
    def set_wheel_velocity(self, l_vel, r_vel):
-      self.write_port('$cDIFFV,{:.0f},{:.0f}'.format(l_vel, r_vel))
+      self.write_port('$cVW,{:.0f},{:.0f}'.format(l_vel, r_vel))
 
 class OMOR1miniNode:
    def __init__(self):
@@ -98,19 +109,26 @@ class OMOR1miniNode:
       self.ph.stop_periodic_comm()  
       
       self.gear_ratio = 21.3
-      self.wheel_distance = 0.17
+      self.wheel_base = 0.17
       self.wheel_radius = 0.03375
+      self.enc_pulse = 44.
 
       self.max_lin_vel_x = 1.2
       self.max_ang_vel_z = 1.0
 
-      self.is_odo_offset_set = False
-      self.odo_offset_left, self.odo_offset_right = 0.0, 0.0
-      self.enc_left_tot_prev, self.enc_right_tot_prev = 0.0, 0.0   
+      self.distance_per_pulse = 2*math.pi*self.wheel_radius / self.enc_pulse / self.gear_ratio
+
+      self.is_enc_offset_reset = False
+      self.left_enc_bias, self.right_enc_bias = 0., 0.
+
+      self.odom_pose = OdomPose()
+      self.odom_vel = OdomVel()
 
       rospy.loginfo('Robot GEAR ratio: %s', self.gear_ratio)
-      rospy.loginfo('Robot wheel_distance: %s', self.wheel_distance)
+      rospy.loginfo('Robot wheel_base: %s', self.wheel_base)
       rospy.loginfo('Robot wheel_radius: %s', self.wheel_radius)
+      rospy.loginfo('Robot enc_pulse: %s', self.enc_pulse)
+      rospy.loginfo('Robot distance_per_pulse: %s', self.distance_per_pulse)
 
       rospy.Service('battery_status', Battery, self.battery_service_handle)
       rospy.Subscriber("cmd_vel", Twist, self.sub_cmd_vel, queue_size=1)
@@ -121,28 +139,64 @@ class OMOR1miniNode:
 
       self.ph.stop_periodic_comm()
       self.ph.update_battery_state()
+      self.ph.set_periodic_info()
 
       sleep(0.1)
 
-      self.ph.set_periodic_info()
-
       rospy.loginfo('==> Start R1mini ')
       rospy.Timer(rospy.Duration(0.01), self.update_robot)
+      self.odom_pose.timestamp = rospy.Time.now().to_nsec()
+
+   def update_odometry(self, odo_l, odo_r, trans_vel, orient_vel):
+      odo_l /= 1000.
+      odo_r /= 1000.
+      theta = (odo_r - odo_l) / self.wheel_base
+      r = (odo_r + odo_l) / 2.0
+
+      timestamp_now = rospy.Time.now()
+      timestamp_now_nsec = timestamp_now.to_nsec()
+
+      self.odom_pose.timestamp = timestamp_now_nsec
+      self.odom_pose.theta = theta
+      self.odom_pose.x = math.cos(theta) * r
+      self.odom_pose.y = math.sin(theta) * r
+
+      self.odom_vel.x = trans_vel / 1000.
+      self.odom_vel.y = 0.0
+      self.odom_vel.w = orient_vel
+
+      parent_frame_id = "odom"
+      child_frame_id = "base_footprint"
+
+      odom_orientation_quat = quaternion_from_euler(0, 0, self.odom_pose.theta)
+      self.odom_broadcaster.sendTransform((self.odom_pose.x, self.odom_pose.y, 0.), odom_orientation_quat, timestamp_now, child_frame_id, parent_frame_id)
+      
+      odom = Odometry()
+      odom.header.stamp = timestamp_now
+      odom.header.frame_id = parent_frame_id
+      odom.child_frame_id = child_frame_id
+      odom.pose.pose = Pose(Point(self.odom_pose.x, self.odom_pose.y, 0.), Quaternion(*odom_orientation_quat))
+      odom.twist.twist = Twist(Vector3(self.odom_vel.x, self.odom_vel.y, 0), Vector3(0, 0, self.odom_vel.w))
+      
+      self.odom_pub.publish(odom)
+
+      print odo_l, odo_r, theta*180./math.pi, self.odom_vel.x*180./math.pi, self.odom_vel.w*180./math.pi/1000.
 
    def update_robot(self, event):
       self.ph.parser_R1mini()
 
       try:
-         [vel_left_wheel, vel_right_wheel] = self.ph.robot_state['DIFFV']
-         [left_odo, right_odo] = self.ph.robot_state['ODO']
-         [left_enc, right_enc] = self.ph.robot_state['ENCOD']
+         [trans_vel, orient_vel] = self.ph.robot_state['VW']
+         [odo_l, odo_r] = self.ph.robot_state['ODO']
+         [gyro_x, gyro_y, gyro_z] = self.ph.robot_state['GYRO']
+         [acc_x, acc_y, acc_z] = self.ph.robot_state['ACCL']
+         [angle_x, angle_y, angle_z] = self.ph.robot_state['POSE']
+
+         self.update_odometry(odo_l, odo_r, trans_vel, orient_vel)
+
       except ValueError:
-         rospy.logwarn("ValueError occupied during read robot status in parser_R1mini. %s ", 
+         rospy.logwarn("ValueError occupied during read robot status in update_robot state. %s ", 
                         self.ph.robot_state)
-
-
-      print self.ph.robot_state
-      print left_odo, right_odo, left_enc, right_enc
       
    def sub_cmd_vel(self, cmd_vel_msg):
       lin_vel_x = cmd_vel_msg.linear.x
@@ -151,11 +205,7 @@ class OMOR1miniNode:
       lin_vel_x = max(-self.max_lin_vel_x, min(self.max_lin_vel_x, lin_vel_x))
       ang_vel_z = max(-self.max_ang_vel_z, min(self.max_ang_vel_z, ang_vel_z))
 
-      vel_l_wheel = (lin_vel_x - (self.wheel_distance / 2.0) * ang_vel_z) / self.wheel_radius
-      vel_r_wheel = (lin_vel_x + (self.wheel_distance / 2.0) * ang_vel_z) / self.wheel_radius
-
-      self.ph.set_wheel_velocity(vel_l_wheel * self.wheel_radius * 1000, 
-                                 vel_r_wheel * self.wheel_radius * 1000)
+      self.ph.set_wheel_velocity(lin_vel_x*1000, ang_vel_z*1000)
 
    def battery_service_handle(self, req):
       self.ph.update_battery_state()
